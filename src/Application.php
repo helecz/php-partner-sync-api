e94373d16ea53f51f014011facdbfb6ea20a9b63
Josef Kříž <pepakriz@gmail.com>
2016-10-17 11:27:20 +0200

    Application: add parser for request headers

--- src/Application.php
+++ src/Application.php
@@ -77,9 +77,31 @@ class Application
 	private function getRequest()
 	{
 		if ($this->request === null) {
-			$this->request = new Request(getallheaders(), file_get_contents('php://input'));
+			$this->request = new Request($this->getHeaders(), file_get_contents('php://input'));
 		}
 		return $this->request;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getHeaders()
+	{
+		if (function_exists('apache_request_headers')) {
+			return apache_request_headers();
+		}
+
+		$headers = array();
+		foreach ($_SERVER as $k => $v) {
+			if (strncmp($k, 'HTTP_', 5) == 0) {
+				$k = substr($k, 5);
+			} elseif (strncmp($k, 'CONTENT_', 8)) {
+				continue;
+			}
+			$headers[strtr($k, '_', '-')] = $v;
+		}
+
+		return $headers;
 	}
 
 }
