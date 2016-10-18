af87a9c0f16fa3c35372690710dd84324285899a
Jan Nedbal <janedbal@gmail.com>
2016-10-18 10:52:14 +0200

    CancelReservationTest: add super simple test

---
+++ tests/src/Method/CancelReservationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace HelePartnerSyncApi\Method;
+
+use DateTime;
+
+class CancelReservationTest extends MethodTestCase
+{
+
+	public function testSuccess()
+	{
+		$startDateTime = new DateTime('+1 hour');
+		$endDateTime = new DateTime('+2 hours');
+		$request = $this->getRequestMock(array(
+			'startDateTime' => $startDateTime->format(DateTime::W3C),
+			'endDateTime' => $endDateTime->format(DateTime::W3C),
+			'quantity' => 1,
+			'parameters' => array(),
+		));
+
+		$method = new CancelReservation(function () {
+			// no action
+		});
+		$response = $method->call($request);
+
+		$this->assertNull($response);
+	}
+
+}
